0ca0e4cf18022e0e11c5cba4ab63345ca68c056b
Michael <mpn123@gmail.com>
2014-04-07 19:21:36 +0200

    added form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,8 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+
+
   # GET /products/1
   # GET /products/1.json
   def show
@@ -64,7 +66,7 @@ class ProductsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
-      @product = Product.find(params[:id])
+      @product = Product.where(name: params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Careerfoundry::Application.routes.draw do
   get 'contact', to: "pages#contact"
   get 'about', to: "pages#about"
 
+  get 'form_input',   to: 'form_example#form_input'
+  get 'form_output',  to: 'form_example#form_output'
+
   resources :products
 
  
