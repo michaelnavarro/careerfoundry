6c806615cc581656dd6d27b531a060065f16627d
Michael <mpn123@gmail.com>
2014-04-14 12:56:37 +0200

    finished post model and added posts to landing_page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   def landing_page
     @featured_product1 = Product.find(1)
     @featured_product2 = Product.find(2)
+
+
+    @latest_posts = Post.all(:limit => 3, :order => "created_at DESC")
   end
 
   def contact
